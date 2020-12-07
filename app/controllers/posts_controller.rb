8455fad1c95c2538645ec71effbc739336b9dc56
akihiro0119 <aono1x19@gmail.com>
2020-12-07 12:36:34 +0900

    新規投稿、詳細、編集、削除　実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
+
   def index
-    @posts = Post.all
+    @posts = Post.includes(:user).order("created_at DESC")
   end
 
   def new
@@ -9,6 +10,28 @@ class PostsController < ApplicationController
 
   def create
     Post.create(post_params)
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+      if @post.update(post_params)
+        redirect_to root_path
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
   end
 
 private
