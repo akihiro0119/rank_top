25fa03affc32178d25324a8dfe84d84d9666ad15
akihiro0119 <aono1x19@gmail.com>
2020-12-08 10:31:11 +0900

    ユーザーページに　名前とプロフィールの出力
    前回のCollectionオプションも解決しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,5 @@
 class UsersController < ApplicationController
   def show
-    user = User.find(params[:id])
-    @name = user.name
-    @posts = user.posts.includes(:user).order("created_at DESC")
+    @user = User.find(params[:id])
   end
 end
