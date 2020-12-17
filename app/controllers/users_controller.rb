cb81eb7c2a83c6d860e158a76f58e808b913b785
akihiro0119 <aono1x19@gmail.com>
2020-12-17 20:12:12 +0900

    フォロー機能、ビューファイルの誤字、実装中のコメントアウト修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,32 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+<<<<<<< Updated upstream
+=======
+
+  def follow
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+    redirect_to user_path(@user)
+  end
+
+
+def unfollow
+    @user = User.find(params[:user_id])
+    current_user.stop_following(@user)
+    redirect_to user_path(@user)
+end
+
+
+
+  def follow_list
+    @user = User.find(params[:user_id])
+  end
+
+
+  def follower_list
+    @user = User.find(params[:user_id])
+  end
+
+>>>>>>> Stashed changes
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,16 @@ Rails.application.routes.draw do
       get 'search'
     end
   end
+<<<<<<< Updated upstream
   
+=======
+
+  put 'users/follow/:user_id' => 'users#follow'
+  put 'users/unfollow/:user_id' => 'users#unfollow'
+
+  
+  get 'users/follow_list/:user_id' => 'users#follow_list'
+  get 'users/follower_list/:user_id' => 'users#follower_list'
+
+>>>>>>> Stashed changes
 end
