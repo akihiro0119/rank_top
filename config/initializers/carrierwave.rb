627c3e92834c4a7d84987927eda236be682f1dac
akihiro0119 <aono1x19@gmail.com>
2020-12-28 02:30:17 +0900

    S3に画像保存のために記述に誤字があったため修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,7 +10,7 @@ require 'carrierwave/storage/fog'
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_ACCESS_KEY_ID'],
-      region: 'ap-northeast-1'
+      region: 'ap-northeast-1',
       path_style: true
 
     }
