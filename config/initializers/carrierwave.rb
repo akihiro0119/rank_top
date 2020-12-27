b929cd1947af0ea4e00b02f32f936bb32c85e766
akihiro0119 <aono1x19@gmail.com>
2020-12-28 02:43:17 +0900

    点のうち忘れ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ unless Rails.env.development? || Rails.env.test?
     
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID']
-      aws_secret_access_key: ENV['AWS_ACCESS_KEY_ID']
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_ACCESS_KEY_ID'],
       region: 'ap-northeast-1'
     }
     config.fog_directory  = 'rank-top-32010'
