454658c91de72d819df10f00394312b9c24fe865
akihiro0119 <aono1x19@gmail.com>
2020-12-28 02:26:41 +0900

    AWSに画像を保存する仕組みの修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,13 +4,9 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
+
     storage :fog
-  end
+
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
